ca737fba0f5f3106f0bd22e8bb736d2d120710c0
GintarePav <gintare.pavilaviciute@gmail.com>
2024-06-03 18:45:14 +0300

    staged 1 commit

--- src/components/BlogSection/BlogSection.jsx
+++ src/components/BlogSection/BlogSection.jsx
@@ -3,7 +3,7 @@ import BlogCommentSection from "../BlogCommentSection/BlogCommentSection";
 const BlogSection = () => {
     return(
         <div className="col-lg-8">
-            <BlogPost />
+            <BlogPost title="Welcome to Blog Post!" subtitle="Posted on January 1, 2023 by Start Bootstrap"/>
             <BlogCommentSection />
         </div>
     )
